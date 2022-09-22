806a93329361da1e0ce9a96bb421c071cb2673c9
Ian Bull <irbull@eclipsesource.com>
2022-09-21 22:25:22 -0700

    Fresh example that uses MUI
    This is an example Fresh [1] project that uses MUI [1] as its widget
    library.
    
    [1] https://fresh.deno.dev/
    [2] https://mui.com/

---
+++ routes/index.tsx
@@ -0,0 +1,15 @@
+import Demo from "../islands/Demo.tsx";
+
+export default function Home() {
+  return (
+    <div>
+      <img
+        src="/logo.svg"
+        width="128"
+        height="128"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
+      <Demo />
+    </div>
+  );
+}
