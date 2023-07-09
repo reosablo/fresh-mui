ac22c57e790364a75655bc4b9a10f0fb0da06312
reosablo <reosablo@users.noreply.github.com>
2023-07-09 14:25:05 +0900

    Remove shims

--- shims/emotion-cache.ts
+++
@@ -1,7 +0,0 @@
-import { IS_BROWSER } from "$fresh/runtime.ts";
-
-const module = IS_BROWSER
-  ? await import("@emotion/cache.browser")
-  : await import("@emotion/cache.worker");
-
-export default module.default;
